1c92b1875830d6d2fb6b6b7014b28b8617bdfdc2
José Caique <caique.alves02@gmail.com>
2026-05-19 23:55:37 -0300

    ✨ feat: atualiza app com rotas, componentes e botões de retorno

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { OrderStatusComponent } from './pages/order-status/order-status.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'status', component: OrderStatusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ web/src/app/components/status-timeline/status-timeline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-timeline',
+  templateUrl: './status-timeline.component.html',
+  styleUrls: ['./status-timeline.component.css']
+})
+export class StatusTimelineComponent {
+
+}
