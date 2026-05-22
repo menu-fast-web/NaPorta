05ad0ed053b755124ec2c127c662b2bbb248911a
José Caique <caique.alves02@gmail.com>
2026-05-22 00:51:38 -0300

    ✨feat: add angular material v16, global body styles and button colors

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { OrderStatusComponent } from './pages/order-status/order-status.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,9 +22,11 @@ import { OrderStatusComponent } from './pages/order-status/order-status.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
