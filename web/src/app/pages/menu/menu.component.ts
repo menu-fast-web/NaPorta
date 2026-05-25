ed6abc33b8c8a8c5d211ba894d3abaa24c287235
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-25 20:26:39 -0300

    🎨 styles: Add refactor at home page with colors new and components adjust

--- web/src/app/pages/menu/menu.component.ts
+++ web/src/app/pages/menu/menu.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  items = Array.from({ length: 10 }, (_, i) => ({
+    id: i + 1,
+    name: 'Filé de frango a milanesa',
+    description: 'Filé de frango a milanesa, servido com arroz e salada',
+    price: 29.90,
+    oldPrice: 39.90,
+    discount: 25,
+    serves: 2,
+    time: 30,
+    image: 'assets/images/card-menu.avif',
+  }));
 }
