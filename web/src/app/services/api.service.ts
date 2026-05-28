dfd13ef56d31d0858c0c2222d0067d975efd8b67
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-28 11:50:21 -0300

    ✨ feat: Add env api and services with methods

---
+++ web/src/app/services/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+// decorator que diz ao angular que a classe pode ser injetada em outros lugares
+@Injectable({
+  providedIn: 'root' // única instância do serviço para toda a aplicação (Singleton)
+})
+export class ApiService {
+  private baseUrl = environment.api;
+  constructor(private http: HttpClient) { }
+
+  // if (!this.baseUrl) {
+  //   throw new Error('API base url not found');
+  // }
+
+  // A generics <T> permite a quem chamar o método dizer qual o tipo de dado espera receber, garante autocomplete e validação de tipos
+  
+  get<T>(path: string) {
+    return this.http.get<T>(`${this.baseUrl}${path}`);
+  }
+
+  post<T>(path: string, body: unknown) {
+    return this.http.post<T>(`${this.baseUrl}${path}`, body);
+  }
+  
+  path<T>(path: string, body: unknown) {
+    return this.http.patch<T>(`${this.baseUrl}${path}`, body);
+  }
+
+  delete<T>(path: string) {
+    return this.http.delete<T>(`${this.baseUrl}${path}`);
+  }
+}
