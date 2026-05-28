8014e5273d931b4effd81f651fe544dad132778d
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-27 21:20:29 -0300

    ✨ feat: Add not found and and sign-in admin

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -6,19 +6,37 @@ import { ProductDetailsComponent } from './pages/product-details/product-details
 import { CartComponent } from './pages/cart/cart.component';
 import { OrderStatusComponent } from './pages/order-status/order-status.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { 
+    path: '', 
+    component: HomeComponent,
+    children: [
+      {
+        path: 'admin',
+        component: AdminLoginComponent
+      }
+    ]
+  },
   {
     path: '',
     component: LayoutComponent,
     children: [
       { path: 'menu', component: MenuComponent },
       { path: 'product/:id', component: ProductDetailsComponent },
+      
       { path: 'cart', component: CartComponent },
       { path: 'status', component: OrderStatusComponent },
-    ]
-  }
+      
+      { path: 'login', component: AdminLoginComponent },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
