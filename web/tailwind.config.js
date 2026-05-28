a3c2fed559f048a8da092ca44ffc5f79706d2e74
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-05-28 09:27:46 -0300

    🐍 fix: Add adjust at preflight tailwindcss

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  corePlugins: {
-    preflight: false,
-  },
+
   theme: { extend: {} },
   plugins: [],
 };
